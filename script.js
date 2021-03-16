8a9f988090c34713d7b26e8d5063db35f9e20566
Smart Coder <69685153+shiva-6214@users.noreply.github.com>
2021-03-16 14:54:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function function_name(argument) {
+	// store input numbers
+let num1 = parseInt(document.getElementById("num1").value);
+let num2 = parseInt(document.getElementById("num2").value);
+
+//add two numbers
+let sum = parseInt(num1) + parseInt(num2);
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+document.getElementById(("ans").innerHTML = ${num1} ${num2} ${sum});
+
+}
